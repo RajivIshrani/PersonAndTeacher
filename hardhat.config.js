de34e5ac18f8f51c8178fd163f944a2e76789a6e
Ishrani Rajiv <rajivishrani@gmail.com>
2022-10-20 17:20:50 +0530

    initial commit

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,27 @@
 require("@nomicfoundation/hardhat-toolbox")
+require("@nomiclabs/hardhat-waffle")
 require("solidity-coverage")
+require("dotenv").config()
+
+const GOERLI_RPC_URL = process.env.GOERLI_RPC_URL
+const MUMBAI_RPC_URL = process.env.MUMBAI_RPC_URL
+const PRIVATE_KEY = process.env.PRIVATE_KEY
 
 module.exports = {
     solidity: "0.8.17",
+    networks: {
+        goerli: {
+            url: GOERLI_RPC_URL,
+            accounts: [PRIVATE_KEY],
+            chainId: 5,
+            blockConfirmations: 6,
+        },
+
+        mumbai: {
+            url: MUMBAI_RPC_URL,
+            accounts: [PRIVATE_KEY],
+            chainId: 80001,
+            blockConfirmations: 6,
+        },
+    },
 }
